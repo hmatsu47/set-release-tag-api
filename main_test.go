4c7019b4f4dce8923729491c28aa6c30f8a88085
hmatsu47 <hmatsu47@gmail.com>
2023-04-03 23:29:39 +0900

    モックテスト準備

--- main_test.go
+++ main_test.go
@@ -7,8 +7,7 @@ import (
 	"testing"
 	"time"
 
-	// "github.com/aws/aws-sdk-go-v2/aws"
-	// "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ecr"
 	"github.com/aws/aws-sdk-go-v2/service/ecr/types"
 	"github.com/deepmap/oapi-codegen/pkg/testutil"
@@ -57,13 +56,13 @@ func TestSetReleaseTag1(t *testing.T) {
 		tag1 := "latest"
 		imageId1 :=
 			types.ImageIdentifier{
-				ImageDigest: &digest1,
-				ImageTag:    &tag1,
+				ImageDigest: aws.String(digest1),
+				ImageTag:    aws.String(tag1),
 			}
 		digest2 := "sha256:20b39162cb057eab7168652ab012ae3712f164bf2b4ef09e6541fca4ead3df62"
 		imageId2 :=
 			types.ImageIdentifier{
-				ImageDigest: &digest2,
+				ImageDigest: aws.String(digest2),
 			}
 		var imageIds []types.ImageIdentifier
 		imageIds = append(imageIds, imageId1)
@@ -80,21 +79,21 @@ func TestSetReleaseTag1(t *testing.T) {
 		tags1 = append(tags1, tag1)
 		imageDetail1 :=
 			types.ImageDetail{
-				ImageDigest:      &digest1,
-				ImagePushedAt:    &expectedTime1,
-				ImageSizeInBytes: &size1Int64,
+				ImageDigest:      aws.String(digest1),
+				ImagePushedAt:    aws.Time(expectedTime1),
+				ImageSizeInBytes: aws.Int64(size1Int64),
 				ImageTags:        tags1,
-				RegistryId:       &registryId,
-				RepositoryName:   &repositoryName,
+				RegistryId:       aws.String(registryId),
+				RepositoryName:   aws.String(repositoryName),
 			}
 		size2Int64 := int64(10017367)
 		imageDetail2 :=
 			types.ImageDetail{
-				ImageDigest:      &digest2,
-				ImagePushedAt:    &expectedTime2,
-				ImageSizeInBytes: &size2Int64,
-				RegistryId:       &registryId,
-				RepositoryName:   &repositoryName,
+				ImageDigest:      aws.String(digest2),
+				ImagePushedAt:    aws.Time(expectedTime2),
+				ImageSizeInBytes: aws.Int64(size2Int64),
+				RegistryId:       aws.String(registryId),
+				RepositoryName:   aws.String(repositoryName),
 			}
 		var imageDetails []types.ImageDetail
 		imageDetails = append(imageDetails, imageDetail1)
@@ -117,15 +116,15 @@ func TestSetReleaseTag1(t *testing.T) {
 		tag1 := "latest"
 		imageId1 :=
 			types.ImageIdentifier{
-				ImageDigest: &digest1,
-				ImageTag:    &tag1,
+				ImageDigest: aws.String(digest1),
+				ImageTag:    aws.String(tag1),
 			}
 		digest2 := "sha256:20b39162cb057eab7168652ab012ae3712f164bf2b4ef09e6541fca4ead3df62"
 		tag2 := "old"
 		imageId2 :=
 			types.ImageIdentifier{
-				ImageDigest: &digest2,
-				ImageTag:    &tag2,
+				ImageDigest: aws.String(digest2),
+				ImageTag:    aws.String(tag2),
 			}
 		var imageIds []types.ImageIdentifier
 		imageIds = append(imageIds, imageId1)
@@ -142,12 +141,12 @@ func TestSetReleaseTag1(t *testing.T) {
 		tags1 = append(tags1, tag1)
 		imageDetail1 :=
 			types.ImageDetail{
-				ImageDigest:      &digest1,
-				ImagePushedAt:    &expectedTime1,
-				ImageSizeInBytes: &size1Int64,
+				ImageDigest:      aws.String(digest1),
+				ImagePushedAt:    aws.Time(expectedTime1),
+				ImageSizeInBytes: aws.Int64(size1Int64),
 				ImageTags:        tags1,
-				RegistryId:       &registryId,
-				RepositoryName:   &repositoryName,
+				RegistryId:       aws.String(registryId),
+				RepositoryName:   aws.String(repositoryName),
 			}
 		size2 := float32(10017367)
 		size2Int64 := int64(10017367)
@@ -155,12 +154,12 @@ func TestSetReleaseTag1(t *testing.T) {
 		tags2 = append(tags2, tag2)
 		imageDetail2 :=
 			types.ImageDetail{
-				ImageDigest:      &digest2,
-				ImagePushedAt:    &expectedTime2,
-				ImageSizeInBytes: &size2Int64,
+				ImageDigest:      aws.String(digest2),
+				ImagePushedAt:    aws.Time(expectedTime2),
+				ImageSizeInBytes: aws.Int64(size2Int64),
 				ImageTags:        tags2,
-				RegistryId:       &registryId,
-				RepositoryName:   &repositoryName,
+				RegistryId:       aws.String(registryId),
+				RepositoryName:   aws.String(repositoryName),
 			}
 		var imageDetails []types.ImageDetail
 		imageDetails = append(imageDetails, imageDetail1)
@@ -182,66 +181,113 @@ func TestSetReleaseTag1(t *testing.T) {
 		assert.Equal(t, tag2, imageList[1].Tags[0])
 	})
 
-	// // モッククライアントの定義
-	// mockECR := &MockECRClient{
-	// 	mockListImages: func(ctx context.Context, params *ecr.ListImagesInput, optFns ...func(*ecr.Options)) (*ecr.ListImagesOutput, error) {
-	// 		RepositoryName := aws.toString(&ecr.ListImagesInput.RepositoryName)
-	// 		RegistryId := aws.toString(&ecr.ListImagesInput.RepositoryName)
-	// 		MaxResults := aws.toString(&ecr.ListImagesInput.RepositoryName)
-	// 		return &ecr.ListImagesOutput{
-	// 			ImageIds: []types.ImageIdentifier{
-	// 				{
-	// 					ImageDigest: aws.String("digest1"),
-	// 					ImageTag:    aws.String("tag1"),
-	// 				},
-	// 				{
-	// 					ImageDigest: aws.String("digest2"),
-	// 					ImageTag:    aws.String("tag2"),
-	// 				},
-	// 			},
-	// 		}, nil
-	// 	},
-	// 	mockDescribeImages: func(ctx context.Context, params *ecr.DescribeImagesInput, optFns ...func(*ecr.Options)) (*ecr.DescribeImagesOutput, error) {
-	// 		return &ecr.DescribeImagesOutput{
-	// 			ImageDetails: []types.ImageDetail{
-	// 				{
-	// 					ImageDigest: aws.String("digest1"),
-	// 					ImageTags:   []string{"tag1"},
-	// 				},
-	// 				{
-	// 					ImageDigest: aws.String("digest2"),
-	// 					ImageTags:   []string{"tag2"},
-	// 				},
-	// 			},
-	// 		}, nil
-	// 	},
-	// 	mockBatchGetImage: func(ctx context.Context, params *ecr.BatchGetImageInput, optFns ...func(*ecr.Options)) (*ecr.BatchGetImageOutput, error) {
-	// 		return &ecr.BatchGetImageOutput{
-	// 			Images: []types.Image{
-	// 				{
-	// 					ImageId: &types.ImageIdentifier{
-	// 						ImageDigest: aws.String("digest1"),
-	// 						ImageTag:    aws.String("tag1"),
+	// t.Run("イメージ取得（モック利用／2つ中1つがタグ付き）", func(t *testing.T) {
+	// 	// テスト用の ListImages の結果を生成
+	// 	digest1 := "sha256:4d2653f861f1c4cb187f1a61f97b9af7adec9ec1986d8e253052cfa60fd7372f"
+	// 	tag1 := "latest"
+	// 	imageId1 :=
+	// 		types.ImageIdentifier{
+	// 			ImageDigest: aws.String(digest1),
+	// 			ImageTag:    aws.String(tag1),
+	// 		}
+	// 	digest2 := "sha256:20b39162cb057eab7168652ab012ae3712f164bf2b4ef09e6541fca4ead3df62"
+	// 	imageId2 :=
+	// 		types.ImageIdentifier{
+	// 			ImageDigest: aws.String(digest2),
+	// 		}
+	// 	var imageIds []types.ImageIdentifier
+	// 	imageIds = append(imageIds, imageId1)
+	// 	imageIds = append(imageIds, imageId2)
+
+	// 	// テスト用の DescribeImages の結果を生成
+	// 	expectedTime1, _ := time.Parse("2006-01-02T15:04:05Z07:00", "2022-09-02T05:27:02Z")
+	// 	expectedTime2, _ := time.Parse("2006-01-02T15:04:05Z07:00", "2022-09-02T05:07:10Z")
+	// 	registryId := "000000000000"
+	// 	repositoryName := "repository1"
+	// 	// size1 := float32(10017365)
+	// 	size1Int64 := int64(10017365)
+	// 	var tags1 []string
+	// 	tags1 = append(tags1, tag1)
+	// 	imageDetail1 :=
+	// 		types.ImageDetail{
+	// 			ImageDigest:      aws.String(digest1),
+	// 			ImagePushedAt:    aws.Time(expectedTime1),
+	// 			ImageSizeInBytes: aws.Int64(size1Int64),
+	// 			ImageTags:        tags1,
+	// 			RegistryId:       aws.String(registryId),
+	// 			RepositoryName:   aws.String(repositoryName),
+	// 		}
+	// 	size2Int64 := int64(10017367)
+	// 	imageDetail2 :=
+	// 		types.ImageDetail{
+	// 			ImageDigest:      aws.String(digest2),
+	// 			ImagePushedAt:    aws.Time(expectedTime2),
+	// 			ImageSizeInBytes: aws.Int64(size2Int64),
+	// 			RegistryId:       aws.String(registryId),
+	// 			RepositoryName:   aws.String(repositoryName),
+	// 		}
+	// 	var imageDetails []types.ImageDetail
+	// 	imageDetails = append(imageDetails, imageDetail1)
+	// 	imageDetails = append(imageDetails, imageDetail2)
+
+	// 	// モッククライアントの定義
+	// 	mockECR := &MockECRClient{
+	// 		mockListImages: func(ctx context.Context, params *ecr.ListImagesInput, optFns ...func(*ecr.Options)) (*ecr.ListImagesOutput, error) {
+	// 			return &ecr.ListImagesOutput{
+	// 				ImageIds: []types.ImageIdentifier{
+	// 					{
+	// 						ImageDigest: aws.String(digest1),
+	// 						ImageTag:    aws.String(tag1),
 	// 					},
-	// 				},
-	// 				{
-	// 					ImageId: &types.ImageIdentifier{
+	// 					{
 	// 						ImageDigest: aws.String("digest2"),
 	// 						ImageTag:    aws.String("tag2"),
 	// 					},
 	// 				},
-	// 			},
-	// 		}, nil
-	// 	},
-	// 	mockPutImage: func(ctx context.Context, params *ecr.PutImageInput, optFns ...func(*ecr.Options)) (*ecr.PutImageOutput, error) {
-	// 		return &ecr.PutImageOutput{
-	// 			Image: &types.Image{
-	// 				ImageId: &types.ImageIdentifier{
-	// 					ImageDigest: aws.String("new-digest"),
-	// 					ImageTag:    aws.String("new-tag"),
+	// 			}, nil
+	// 		},
+	// 		mockDescribeImages: func(ctx context.Context, params *ecr.DescribeImagesInput, optFns ...func(*ecr.Options)) (*ecr.DescribeImagesOutput, error) {
+	// 			return &ecr.DescribeImagesOutput{
+	// 				ImageDetails: []types.ImageDetail{
+	// 					{
+	// 						ImageDigest: aws.String("digest1"),
+	// 						ImageTags:   []string{"tag1"},
+	// 					},
+	// 					{
+	// 						ImageDigest: aws.String("digest2"),
+	// 						ImageTags:   []string{"tag2"},
+	// 					},
 	// 				},
-	// 			},
-	// 		}, nil
-	// 	},
-	// }
+	// 			}, nil
+	// 		},
+	// 		mockBatchGetImage: func(ctx context.Context, params *ecr.BatchGetImageInput, optFns ...func(*ecr.Options)) (*ecr.BatchGetImageOutput, error) {
+	// 			return &ecr.BatchGetImageOutput{
+	// 				Images: []types.Image{
+	// 					{
+	// 						ImageId: &types.ImageIdentifier{
+	// 							ImageDigest: aws.String("digest1"),
+	// 							ImageTag:    aws.String("tag1"),
+	// 						},
+	// 					},
+	// 					{
+	// 						ImageId: &types.ImageIdentifier{
+	// 							ImageDigest: aws.String("digest2"),
+	// 							ImageTag:    aws.String("tag2"),
+	// 						},
+	// 					},
+	// 				},
+	// 			}, nil
+	// 		},
+	// 		mockPutImage: func(ctx context.Context, params *ecr.PutImageInput, optFns ...func(*ecr.Options)) (*ecr.PutImageOutput, error) {
+	// 			return &ecr.PutImageOutput{
+	// 				Image: &types.Image{
+	// 					ImageId: &types.ImageIdentifier{
+	// 						ImageDigest: aws.String("new-digest"),
+	// 						ImageTag:    aws.String("new-tag"),
+	// 					},
+	// 				},
+	// 			}, nil
+	// 		},
+	// 	}
+	// })
 }
