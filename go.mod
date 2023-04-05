0d31b02e71464cb4c58ad44719a5866b04805a34
hmatsu47 <hmatsu47@gmail.com>
2023-04-05 16:54:10 +0900

    一旦モックテストを外す

--- go.mod
+++ go.mod
@@ -38,11 +38,8 @@ require (
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
-	github.com/labstack/echo/v4 v4.9.1 // indirect
-	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -51,8 +48,6 @@ require (
 	github.com/perimeterx/marshmallow v1.1.4 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
--- main_test.go
+++ main_test.go
@@ -1,52 +1,14 @@
 package main
 
 import (
-	"context"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/ecr"
 	"github.com/aws/aws-sdk-go-v2/service/ecr/types"
-	"github.com/deepmap/oapi-codegen/pkg/testutil"
 	"github.com/go-playground/assert/v2"
 	"github.com/hmatsu47/set-release-tag-api/api"
 )
-
-func doGet(t *testing.T, handler http.Handler, url string) *httptest.ResponseRecorder {
-	response := testutil.NewRequest().Get(url).WithAcceptJson().GoWithHTTPHandler(t, handler)
-	return response.Recorder
-}
-
-// モック化
-type ECRClientAPI interface {
-	ListImages(ctx context.Context, params *ecr.ListImagesInput, optFns ...func(*ecr.Options)) (*ecr.ListImagesOutput, error)
-	DescribeImages(ctx context.Context, params *ecr.DescribeImagesInput, optFns ...func(*ecr.Options)) (*ecr.DescribeImagesOutput, error)
-	BatchGetImage(ctx context.Context, params *ecr.BatchGetImageInput, optFns ...func(*ecr.Options)) (*ecr.BatchGetImageOutput, error)
-	PutImage(ctx context.Context, params *ecr.PutImageInput, optFns ...func(*ecr.Options)) (*ecr.PutImageOutput, error)
-}
-
-type MockECRClient struct {
-	ECRClientAPI
-	mockListImages     func(ctx context.Context, params *ecr.ListImagesInput, optFns ...func(*ecr.Options)) (*ecr.ListImagesOutput, error)
-	mockDescribeImages func(ctx context.Context, params *ecr.DescribeImagesInput, optFns ...func(*ecr.Options)) (*ecr.DescribeImagesOutput, error)
-	mockBatchGetImage  func(ctx context.Context, params *ecr.BatchGetImageInput, optFns ...func(*ecr.Options)) (*ecr.BatchGetImageOutput, error)
-	mockPutImage       func(ctx context.Context, params *ecr.PutImageInput, optFns ...func(*ecr.Options)) (*ecr.PutImageOutput, error)
-}
-
-func (m *MockECRClient) ListImages(ctx context.Context, params *ecr.ListImagesInput, optFns ...func(*ecr.Options)) (*ecr.ListImagesOutput, error) {
-	return m.mockListImages(ctx, params, optFns...)
-}
-
-func (m *MockECRClient) DescribeImages(ctx context.Context, params *ecr.DescribeImagesInput, optFns ...func(*ecr.Options)) (*ecr.DescribeImagesOutput, error) {
-	return m.mockDescribeImages(ctx, params, optFns...)
-}
-
-func (m *MockECRClient) BatchGetImage(ctx context.Context, params *ecr.BatchGetImageInput, optFns ...func(*ecr.Options)) (*ecr.BatchGetImageOutput, error) {
-	return m.mockBatchGetImage(ctx, params, optFns...)
-}
 
 // go test -v で実行する
 func TestSetReleaseTag1(t *testing.T) {
@@ -180,116 +142,4 @@ func TestSetReleaseTag1(t *testing.T) {
 		assert.Equal(t, 1, len(imageList[1].Tags))
 		assert.Equal(t, tag2, imageList[1].Tags[0])
 	})
-}
-
-func TestSetReleaseTag2(t *testing.T) {
-	// t.Run("イメージ取得（モック利用／2つ中1つがタグ付き）", func(t *testing.T) {
-	// 	// テスト用の ListImages の結果を生成
-	// 	digest1 := "sha256:4d2653f861f1c4cb187f1a61f97b9af7adec9ec1986d8e253052cfa60fd7372f"
-	// 	tag1 := "latest"
-	// 	imageId1 :=
-	// 		types.ImageIdentifier{
-	// 			ImageDigest: aws.String(digest1),
-	// 			ImageTag:    aws.String(tag1),
-	// 		}
-	// 	digest2 := "sha256:20b39162cb057eab7168652ab012ae3712f164bf2b4ef09e6541fca4ead3df62"
-	// 	imageId2 :=
-	// 		types.ImageIdentifier{
-	// 			ImageDigest: aws.String(digest2),
-	// 		}
-	// 	var imageIds []types.ImageIdentifier
-	// 	imageIds = append(imageIds, imageId1)
-	// 	imageIds = append(imageIds, imageId2)
-
-	// 	// テスト用の DescribeImages の結果を生成
-	// 	expectedTime1, _ := time.Parse("2006-01-02T15:04:05Z07:00", "2022-09-02T05:27:02Z")
-	// 	expectedTime2, _ := time.Parse("2006-01-02T15:04:05Z07:00", "2022-09-02T05:07:10Z")
-	// 	registryId := "000000000000"
-	// 	repositoryName := "repository1"
-	// 	// size1 := float32(10017365)
-	// 	size1Int64 := int64(10017365)
-	// 	var tags1 []string
-	// 	tags1 = append(tags1, tag1)
-	// 	imageDetail1 :=
-	// 		types.ImageDetail{
-	// 			ImageDigest:      aws.String(digest1),
-	// 			ImagePushedAt:    aws.Time(expectedTime1),
-	// 			ImageSizeInBytes: aws.Int64(size1Int64),
-	// 			ImageTags:        tags1,
-	// 			RegistryId:       aws.String(registryId),
-	// 			RepositoryName:   aws.String(repositoryName),
-	// 		}
-	// 	size2Int64 := int64(10017367)
-	// 	imageDetail2 :=
-	// 		types.ImageDetail{
-	// 			ImageDigest:      aws.String(digest2),
-	// 			ImagePushedAt:    aws.Time(expectedTime2),
-	// 			ImageSizeInBytes: aws.Int64(size2Int64),
-	// 			RegistryId:       aws.String(registryId),
-	// 			RepositoryName:   aws.String(repositoryName),
-	// 		}
-	// 	var imageDetails []types.ImageDetail
-	// 	imageDetails = append(imageDetails, imageDetail1)
-	// 	imageDetails = append(imageDetails, imageDetail2)
-
-	// 	// モッククライアントの定義
-	// 	mockECR := &MockECRClient{
-	// 		mockListImages: func(ctx context.Context, params *ecr.ListImagesInput, optFns ...func(*ecr.Options)) (*ecr.ListImagesOutput, error) {
-	// 			return &ecr.ListImagesOutput{
-	// 				ImageIds: []types.ImageIdentifier{
-	// 					{
-	// 						ImageDigest: aws.String(digest1),
-	// 						ImageTag:    aws.String(tag1),
-	// 					},
-	// 					{
-	// 						ImageDigest: aws.String("digest2"),
-	// 						ImageTag:    aws.String("tag2"),
-	// 					},
-	// 				},
-	// 			}, nil
-	// 		},
-	// 		mockDescribeImages: func(ctx context.Context, params *ecr.DescribeImagesInput, optFns ...func(*ecr.Options)) (*ecr.DescribeImagesOutput, error) {
-	// 			return &ecr.DescribeImagesOutput{
-	// 				ImageDetails: []types.ImageDetail{
-	// 					{
-	// 						ImageDigest: aws.String("digest1"),
-	// 						ImageTags:   []string{"tag1"},
-	// 					},
-	// 					{
-	// 						ImageDigest: aws.String("digest2"),
-	// 						ImageTags:   []string{"tag2"},
-	// 					},
-	// 				},
-	// 			}, nil
-	// 		},
-	// 		mockBatchGetImage: func(ctx context.Context, params *ecr.BatchGetImageInput, optFns ...func(*ecr.Options)) (*ecr.BatchGetImageOutput, error) {
-	// 			return &ecr.BatchGetImageOutput{
-	// 				Images: []types.Image{
-	// 					{
-	// 						ImageId: &types.ImageIdentifier{
-	// 							ImageDigest: aws.String("digest1"),
-	// 							ImageTag:    aws.String("tag1"),
-	// 						},
-	// 					},
-	// 					{
-	// 						ImageId: &types.ImageIdentifier{
-	// 							ImageDigest: aws.String("digest2"),
-	// 							ImageTag:    aws.String("tag2"),
-	// 						},
-	// 					},
-	// 				},
-	// 			}, nil
-	// 		},
-	// 		mockPutImage: func(ctx context.Context, params *ecr.PutImageInput, optFns ...func(*ecr.Options)) (*ecr.PutImageOutput, error) {
-	// 			return &ecr.PutImageOutput{
-	// 				Image: &types.Image{
-	// 					ImageId: &types.ImageIdentifier{
-	// 						ImageDigest: aws.String("new-digest"),
-	// 						ImageTag:    aws.String("new-tag"),
-	// 					},
-	// 				},
-	// 			}, nil
-	// 		},
-	// 	}
-	// })
 }
